8dad40d2f21d420e5a9bb5db5866b0627282e617
logeshns <124495107+logeshns@users.noreply.github.com>
2026-06-07 22:55:51 +0530

    Add: depth-of-binary-tree - submission-0

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-0.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public int maxDepth(TreeNode root) {
+        return depth(root);
+    }
+    public int depth(TreeNode node){
+        if(node==null){
+            return 0;
+        }
+        int x=depth(node.left);
+        int y=depth(node.right);
+        return 1+Math.max(x,y);
+    }
+}
