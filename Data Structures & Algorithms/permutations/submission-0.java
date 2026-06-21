fc8a34a34c05592b07295145bee50beb5d9034d4
logeshns <124495107+logeshns@users.noreply.github.com>
2026-06-21 19:30:57 +0530

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.java
@@ -0,0 +1,33 @@
+class Solution {
+
+    public List<List<Integer>> permute(int[] nums) {
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        backtrack(result, new ArrayList<>(), nums);
+
+        return result;
+    }
+
+    private void backtrack(List<List<Integer>> resultList,
+                           ArrayList<Integer> tempList,
+                           int[] nums) {
+
+        if (tempList.size() == nums.length) {
+            resultList.add(new ArrayList<>(tempList));
+            return;
+        }
+
+        for (int number : nums) {
+
+            if (tempList.contains(number))
+                continue;
+
+            tempList.add(number);
+
+            backtrack(resultList, tempList, nums);
+
+            tempList.remove(tempList.size() - 1);
+        }
+    }
+}
