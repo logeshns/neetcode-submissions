b65b5818fd4a482fdf5730b0f2bc6cf57fc8dced
logeshns <124495107+logeshns@users.noreply.github.com>
2026-07-07 03:41:30 +0530

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.java
@@ -0,0 +1,18 @@
+class Solution {
+    int[] cache;
+    public int rob(int[] nums) {
+        cache=new int[nums.length];
+        Arrays.fill(cache,-1);
+        return dfs(nums,0);
+    }
+    public int dfs(int[] nums,int i){
+        if(i>=nums.length){
+            return 0;
+        }
+        if(cache[i]!=-1){
+            return cache[i];
+        }
+        cache[i] =Math.max(nums[i]+dfs(nums,i+2),dfs(nums,i+1));
+        return cache[i];
+    }
+}
