7994896b045c0b4788101f1cbef63f150a61c339
logeshns <124495107+logeshns@users.noreply.github.com>
2026-07-10 00:58:20 +0530

    Add: maximum-subarray - submission-0

---
+++ Data Structures & Algorithms/maximum-subarray/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int curr=0,max=Integer.MIN_VALUE;
+        for(int num:nums){
+            curr+=num;
+            max=Math.max(curr,max);
+            if(curr<0){
+                curr=0;
+            }
+        }
+        return max;
+    }
+}
